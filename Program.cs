9b2598c64e4571b2c6a598bdb63ab3d6c4f34f9d
Tyler Powewr <42220736+FoolishProff@users.noreply.github.com>
2018-08-24 23:51:22 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace WordCountTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a valid text path: ");
+            string filename = Console.ReadLine();
+            StreamReader read = new StreamReader(filename);
+
+            string contents = read.ReadToEnd();
+
+            string [] words = contents.Split(' ');
+
+            int count = 0;
+
+            foreach(string c in words)
+            {
+                count++;
+            }
+
+
+            Console.WriteLine("Word Count: {0}",count);
+
+
+        }
+    }
+}
